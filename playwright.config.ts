56cf6d03b8b0360ad10af2ffc9c647d125262d68
Sam <drobbybo21@gmail.com>
2026-05-17 22:58:37 +0300

    Первые проверки и начальные тесты для хэдера сайта

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,10 +26,14 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: 'https://automationexercise.com',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    extraHTTPHeaders: {
+      'Cache-Control': 'no-cache',
+      Pragma: 'no-cache',
+    },
   },
 
   /* Configure projects for major browsers */
